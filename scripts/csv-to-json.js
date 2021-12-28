ec328ac2d369eea6f89144ce360596726b1dbf83
Oliver Nordbjerg <hi@notbjerg.me>
2021-12-28 04:59:49 +0100

    Add script to convert whitelist CSV to JSON

---
+++ scripts/csv-to-json.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const csv = require('csv-parser')
+
+const whitelist = {}
+fs.createReadStream('genesis.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row)
+    whitelist[row['Address']] = row['Number']
+  })
+  .on('end', () => {
+    fs.writeFileSync(
+      'whitelist.json',
+      JSON.stringify(whitelist, null, 4)
+    )
+  })
